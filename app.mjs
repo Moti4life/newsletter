3e52e2fe9de1829e3128cc1327a834caa4d9cea3
unknown <tim.manankil@gmail.com>
2021-06-09 19:11:11 +0800

    err3

--- app.mjs
+++ app.mjs
@@ -29,8 +29,8 @@ mailChimp.setConfig({
 const newSubs = async ( listID, newUser) => {
     const checkmail = await mailChimp.ping.get()
     console.log(checkmail)
-    // console.log(process.env.AUDIENCEID)
-    // console.log(process.env.CHIMPAPIKEY)
+    console.log(process.env.AUDIENCEID)
+    console.log(process.env.CHIMPAPIKEY)
     try {
         const response = await mailChimp.lists.addListMember(listID,{
             email_address: newUser.email,
